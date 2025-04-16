d4451d53fc45740319085f3b848d34896a5c4d64
thuongtms <thuong.pham@tomosia.com>
2025-04-16 15:08:44 +0700

    feature/bai1

---
+++ week1/src/components/atoms/ClassTextComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="class-text">
+    <p :class="{ 'class-text__red': isRed }">
+      Xin chào Vue 3!
+    </p>
+    <button @click="color" class="class-text__button">
+      Đổi màu
+    </button>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+  const isRed = ref(false)
+  const color = () => {
+    isRed.value = !isRed.value
+  }
+</script>
+
+
+<style lang="scss" scoped>
+  @use "../../assets/style/page/_classText.scss" as *;
+</style>
---
+++ week1/src/views/ClassTextView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="class-text-view">
+    <ClassText />
+  </div>
+</template>
+
+<script setup>
+  import ClassText from '../components/atoms/ClassTextComponent.vue'
+</script>
