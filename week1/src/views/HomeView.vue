8ede6f32abbbd615e35fdc761178ceb308a2fccd
thuongtms <thuong.pham@tomosia.com>
2025-04-16 11:03:35 +0700

    feature/bai3

--- week1/src/views/HomeView.vue
+++ week1/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
     <HtmlTextDemo />
+    <ButtonComponent />
   </div>
 </template>
 
 <script setup>
   import HtmlTextDemo from '../components/atoms/HtmlAndTextComponent.vue'
+  import ButtonComponent from '../components/atoms/ButtonComponent.vue'
 </script>
