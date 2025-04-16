ed9cd1450579f7f5576ef213203a2a5fa0ab2804
thuongtms <thuong.pham@tomosia.com>
2025-04-16 15:26:26 +0700

    feature/bai2

--- week1/src/components/atoms/ClassTextComponent.vue
+++ week1/src/components/atoms/ClassTextComponent.vue
@@ -3,20 +3,24 @@
     <p :class="{ 'class-text__red': isRed }">
       Xin chào Vue 3!
     </p>
-    <button @click="color" class="class-text__button">
+    <button @click="textColor" class="class-text__button">
       Đổi màu
     </button>
+
+    <div class="class-text__box" :style="{ backgroundColor: bgColor }"></div>
+    <label for="bg-color-picker" class="class-text__label">màu nền</label>
+    <input id="bg-color-picker" type="color" v-model="bgColor" class="class-text__color-picker" />
   </div>
 </template>
 
 <script setup>
   import { ref } from 'vue'
   const isRed = ref(false)
-  const color = () => {
+  const textColor = () => {
     isRed.value = !isRed.value
   }
+  const bgColor = ref('#288628')
 </script>
-
 
 <style lang="scss" scoped>
   @use "../../assets/style/page/_classText.scss" as *;
