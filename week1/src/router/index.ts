6e2a20a069dde8809b07b7171447c76ef1b7a817
thuongtms <thuong.pham@tomosia.com>
2025-04-18 14:54:18 +0700

    feature/bai2

--- week1/src/router/index.ts
+++ week1/src/router/index.ts
@@ -3,7 +3,8 @@ import HomeView from '@/views/HomeView.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: HomeView },
-  { path: '/exe-six-one', name: 'User', component: () => import('@/views/UserView.vue') }
+  { path: '/exe-six-one', name: 'User', component: () => import('@/views/UserView.vue') },
+  { path: '/exe-six-two', name: 'Counter', component: () => import('@/views/CounterView.vue') }
 ]
 
 const router = createRouter({
