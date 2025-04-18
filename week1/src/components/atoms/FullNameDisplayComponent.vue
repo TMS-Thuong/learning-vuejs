336ee5f24ca3df41965145e5e3aba4b452da3934
thuongtms <thuong.pham@tomosia.com>
2025-04-18 08:58:02 +0700

    feature/bai1

---
+++ week1/src/components/atoms/FullNameDisplayComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <p class="full-name-display">{{ fullName }}</p>
+</template>
+
+<script setup>
+const props = defineProps({
+  fullName: {
+    type: String,
+    required: true
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+@use "../../assets/styles/_full-name-display.scss" as *;
+</style>
