bdc86835aa145d61896191baca2492abd685bbd2
thuongtms <thuong.pham@tomosia.com>
2025-04-18 11:05:32 +0700

    feature/bai3

---
+++ week1/src/components/molecules/CountWatchComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="counter">
+    <button class="counter__btn" @click="onIncrement">Tăng</button>
+    <CountDisplayComponent :count="count" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+import CountDisplayComponent from '../atoms/CountDisplayComponent.vue';
+
+const count = ref(0);
+
+watch(count, (newVal) => {
+  console.log('Giá trị mới:'+ newVal);
+});
+
+function onIncrement() {
+  count.value++;
+}
+</script>
+
+<style lang="scss" scoped>
+@use "../../assets/styles/_count-watch.scss" as *;
+</style>
--- week1/src/router/routerNames.ts
+++ week1/src/router/routerNames.ts
@@ -2,4 +2,5 @@ export enum ROUTE_NAMES {
   HOME = 'HomeView',
   FULL_NAME = 'FullNameView',
   CHAR_COUNT = 'CharCountView',
+  WATCH_COUNT = 'WatchCount'
 }
