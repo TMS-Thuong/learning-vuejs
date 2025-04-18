266d1bf7b713c5bac01fede8758a9e28b177d248
thuongtms <thuong.pham@tomosia.com>
2025-04-18 13:06:36 +0700

    feature/bai1

---
+++ week1/src/components/molecules/ManageUserComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="manage-user">
+    <HelloUser :name="userName" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import HelloUser from '../atoms/HelloUserComponent.vue'
+
+const userName = 'An';
+</script>
+
---
+++ week1/src/views/UserView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="user-view">
+    <User />
+  </div>
+</template>
+
+<script setup>
+import User from '../components/molecules/ManageUserComponent.vue'
+</script>
